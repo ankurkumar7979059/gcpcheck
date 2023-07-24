3265b1f73e9240625ab87958a6401eec385a1b78
ankurkumar7979059 <126285434+ankurkumar7979059@users.noreply.github.com>
2023-07-25 02:11:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "google_storage_bucket" "auto-expire" {
   name = "abcdefg1235"
   location ="US"
 }
+provider "google" {
+  project     =  "terraform-390319"
+  region      = "us-central1"
+  zone        =  "us-central1-a"
+
+
+}
